458e7538d6337f07b021c842b30f555202c3f1f0
CptKekoa <149622803+CptKekoa@users.noreply.github.com>
2024-03-27 11:26:18 -0400

    Add files via upload

---
+++ Add.py
@@ -0,0 +1,18 @@
+from Location import Location
+from Item import Item
+
+# get all item inputs
+item_name, item_id, item_quantity, item_weight = input("Enter item name, id, quantity, and weight all separated by spaces: ").split()
+
+# create item
+new_item = Item(item_name, item_id, item_quantity, item_weight)
+
+# get location inputs
+location_row, location_shelf, location_bin = input("Enter location row, shelf, and bin all separated by spaces: ").split()
+
+# create location
+new_location = Location(location_row, location_shelf, location_bin)
+
+# display new Item and Location data
+new_item.myFunc()
+new_location.myFunc()
