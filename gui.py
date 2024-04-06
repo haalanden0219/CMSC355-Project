e36ffc3c24c3abab792d5e9a04cb58b11a309bec
Evan <haalanden@vcu.edu>
2024-04-06 15:08:40 -0400

    organized gui, updated readme

--- gui.py
+++ gui.py
@@ -20,10 +20,10 @@ def bin_search_button_clicked():
 def add_item_button_clicked():
     global counter
     counter += 1
-    item_name = item_name_entry.get()
-    item_id = item_id_entry.get()
-    item_quantity = item_quantity_entry.get()
-    item_weight = item_weight_entry.get()
+    item_name = add_item_name_entry.get()
+    item_id = add_item_id_entry.get()
+    item_quantity = add_item_quantity_entry.get()
+    item_weight = add_item_weight_entry.get()
 
     new_item = Item(item_name, item_id, item_quantity, item_weight)
     new_item.print_out()
@@ -33,48 +33,83 @@ def add_item_button_clicked():
     item_add_success = f"{item_name} is succesfully saved"
     messagebox.showinfo("Success", item_add_success)
 
+def delete_item_button_clicked():
+    print("delete button pressed")
+
+def print_location_button_clicked():
+    print("delete button pressed")
+
 
 global warehouse_object
 warehouse_object = [Location(1, 1, 1), Location(1, 1, 2)]
 
 root = tk.Tk()
 root.title("Warehouse Inventory system")
-root.geometry("500x500")
+root.geometry("1200x1200")
 
+#Button Creation
 bin_search_button = tk.Button(root, text="Search Bin", command=bin_search_button_clicked)
 add_item_button = tk.Button(root, text="Add Item to Bin", command=add_item_button_clicked)
+delete_item_button = tk.Button(root, text="Delete Item", command= delete_item_button_clicked)
+print_location_button = tk.Button(root, text="Print Location", command= print_location_button_clicked)
 
+
+#bin_search entry creation
 bin_search_name_entry = tk.Entry(root)
 bin_search_id_entry = tk.Entry(root)
 
-item_name_entry = tk.Entry(root)
-item_id_entry = tk.Entry(root)
-item_quantity_entry = tk.Entry(root)
-item_weight_entry = tk.Entry(root)
+#add_item entry creation
+add_item_name_entry = tk.Entry(root)
+add_item_id_entry = tk.Entry(root)
+add_item_quantity_entry = tk.Entry(root)
+add_item_weight_entry = tk.Entry(root)
 
+#delete_item entry creation
+delete_item_name_entry = tk.Entry(root)
+delete_item_id_entry = tk.Entry(root)
+
+
+#bin_search label creation
 bin_search_name_label = tk.Label(root, text="Item Name:")
 bin_search_id_label = tk.Label(root, text="Item ID:")
 
-item_name_label = tk.Label(root, text="Item Name:")
-item_id_label = tk.Label(root, text="Item ID:")
-item_quantity_label = tk.Label(root, text="Item Quantity:")
-item_weight_label = tk.Label(root, text="Item Weight:")
+#add_item label creation
+add_item_name_label = tk.Label(root, text="Item Name:")
+add_item_id_label = tk.Label(root, text="Item ID:")
+add_item_quantity_label = tk.Label(root, text="Item Quantity:")
+add_item_weight_label = tk.Label(root, text="Item Weight:")
 
-bin_search_button.pack(side=tk.TOP, padx=5, pady=5, anchor=tk.CENTER)
-add_item_button.pack(side=tk.LEFT, padx=5, pady=5, anchor=tk.CENTER)
+#delete_item label creation
+delete_item_name_label = tk.Label(root, text="Item Name:")
+delete_item_id_label = tk.Label(root, text="Item ID:")
 
-bin_search_name_label.pack(side=tk.TOP, padx=5, pady=5, anchor=tk.CENTER)
-bin_search_name_entry.pack(side=tk.TOP, padx=5, pady=5, anchor=tk.CENTER)
-bin_search_id_label.pack(side=tk.TOP, padx=5, pady=5, anchor=tk.CENTER)
-bin_search_id_entry.pack(side=tk.TOP, padx=5, pady=5, anchor=tk.CENTER)
+#buttons layout
+bin_search_button.place( x = 300 , y = 300 , width = 100)
+add_item_button.place(x = 300 , y = 350 , width=100)
+delete_item_button.place(x = 300 , y = 400 , width=100)
+print_location_button.place(x = 300 , y = 450 , width=100)
 
-item_name_label.pack(side=tk.LEFT, padx=5, pady=5)
-item_name_entry.pack(side=tk.LEFT, padx=5, pady=5)
-item_id_label.pack(side=tk.LEFT, padx=5, pady=5)
-item_id_entry.pack(side=tk.LEFT, padx=5, pady=5)
-item_quantity_label.pack(side=tk.LEFT, padx=5, pady=5)
-item_quantity_entry.pack(side=tk.LEFT, padx=5, pady=5)
-item_weight_label.pack(side=tk.LEFT, padx=5, pady=5)
-item_weight_entry.pack(side=tk.LEFT, padx=5, pady=5)
+
+#bin search layout
+bin_search_name_label.place(x = 400 , y = 300 , width = 100)
+bin_search_name_entry.place(x = 500 , y = 300 , width = 100)
+bin_search_id_label.place(x = 600 , y = 300 , width = 100)
+bin_search_id_entry.place(x = 700 , y = 300 , width = 100)
+
+#add item layout
+add_item_name_label.place(x = 400, y = 350 , width=100)
+add_item_name_entry.place(x = 500, y = 350 , width=100)
+add_item_id_label.place(x = 600, y = 350 , width=100)
+add_item_id_entry.place(x = 700, y = 350 , width=100)
+add_item_quantity_label.place(x = 800, y = 350 , width=100)
+add_item_quantity_entry.place(x = 900, y = 350 , width=100)
+add_item_weight_label.place(x = 1000, y = 350 , width=100)
+add_item_weight_entry.place(x = 1100, y = 350 , width=100)
+
+#delete item layout
+delete_item_name_label.place(x = 400, y = 400 , width=100)
+delete_item_name_entry.place(x = 500, y = 400 , width=100)
+delete_item_id_label.place(x = 600, y = 400 , width=100)
+delete_item_id_entry.place(x = 700, y = 400 , width=100)
 
 root.mainloop()
